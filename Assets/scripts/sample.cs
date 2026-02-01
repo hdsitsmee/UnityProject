202a8078f80b2d45ee75a4553077b482fd12a85a
hdsitsmee <dasom06262@naver.com>
2026-02-01 18:08:56 +0900

    .
    .

--- Assets/scripts/sample.cs
+++ Assets/scripts/sample.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-//테스트 줄 추후 삭제
+//테스트 줄 추후 삭제.
 public class sample : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
