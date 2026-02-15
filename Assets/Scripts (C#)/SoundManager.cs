afe214d5fa06a3b2a318569ee4bb929b2cf48196
hdsitsmee <dasom06262@naver.com>
2026-02-15 17:26:16 +0900

    feat: make main scene 사운드 추가
    -던전 버튼
    -make 버튼
    -도감 열고/닫기 버튼
    -도감 탭 버튼
    -finish 버튼
    -재료 버튼
    -카페 배경 음악
    
    -도감 버튼 sprite 수정

---
+++ Assets/Scripts (C#)/SoundManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    [Header("스피커 설정")]
+    public AudioSource bgmPlayer;//배경음악용 스피커
+    public AudioSource sfxPlayer;//효과음용 스피커
+
+    [Header("배경음악 파일")]
+    public AudioClip cafeBgm;//카페 배경음악
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        //게임 시작하자마자 카페 재생
+        PlayBGM(cafeBgm);
+    }
+
+    //배경음악 재생 함수
+    public void PlayBGM(AudioClip clip)
+    {
+        if (clip == null) return;
+        
+        bgmPlayer.clip = clip;
+        bgmPlayer.loop = true; //무한 반복
+        bgmPlayer.Play();
+    }
+
+    //효과음 재생 함수
+    public void PlaySFX(AudioClip clip)
+    {
+        if (clip == null) return;
+        sfxPlayer.PlayOneShot(clip); 
+    }
+}
